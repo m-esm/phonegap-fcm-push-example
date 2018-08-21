e563644fcb525b58fc1d03333959c1a7de9bdea6
Mohsen Esmaeli <m-esm@hotmail.com>
2018-08-22 02:33:28 +0430

    log

--- www/js/index.js
+++ www/js/index.js
@@ -1,22 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 function httpPost(url, json) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
@@ -26,68 +7,36 @@ function httpPost(url, json) {
 
 function log(input) {
     console.log(input);
-    httpPost('http://20.30.0.92:2020/echo', input)
+    document.getElementById('push_token').innerHTML += JSON.stringify(input) + '\n\n';
 }
 
-log({ message: 'hello server' });
+log({ message: 'hello' });
 
-var app = {
-    // Application Constructor
-    initialize: function () {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function () {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+document.addEventListener('deviceready', onDeviceReady, false);
+function onDeviceReady() {
 
+    log({ message: 'device is ready' });
 
+    const push = PushNotification.init({
+        android: {},
+        browser: {
+            pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+        },
+        ios: {
+            alert: 'true',
+            badge: true,
+            sound: 'false'
+        },
+        windows: {}
+    });
 
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function () {
-        app.receivedEvent('deviceready');
+    push.on('registration', data => {
+        log(data);
+    });
 
-        log({ message: 'device is ready' });
-
-        const push = PushNotification.init({
-            android: {},
-            browser: {
-                pushServiceURL: 'http://push.api.phonegap.com/v1/push'
-            },
-            ios: {
-                alert: 'true',
-                badge: true,
-                sound: 'false'
-            },
-            windows: {}
-        });
-
-        push.on('registration', data => {
-            log(data);
-            document.getElementById('push_token').innerHTML = data.registrationId;
-        });
-
-        push.on('error', e => {
-            log(e);
-        });
+    push.on('error', e => {
+        log(e);
+    });
 
 
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function (id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
+}
